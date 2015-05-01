605bd97d12aaee9ed6e4709ef75ed303d5e906af
BigIrishLion <bigirishlion@yahoo.com>
2015-05-01 14:54:46 -0700

    Removed some commented code

--- url-ajax-page.php
+++ url-ajax-page.php
@@ -2,26 +2,12 @@
 
 require_once('curl-class.php');
 
-/*
-http://www.w3schools.com/
-http://themeforest.net/asasasdasdas
-http://themeforest.net/
-http://google.com/
-*/
-
-// echo "<br />";
 if (isset($_POST['submit'])) {
 
 	$url = $_POST['submit'];
-	//$urlSuccess = get_headers($url,1);
 
 	$cc = new myCurl($url);
 	$cc->createCurl();
-
-	/*echo '<tr>';
-	echo '<td>'. $url. '</td>';
-	echo '<td>'. $urlSuccess[0] . '</td>';
-	echo '</tr>';*/
 
 	echo '<tr>';
 	echo '<td>'. $url. '</td>';
